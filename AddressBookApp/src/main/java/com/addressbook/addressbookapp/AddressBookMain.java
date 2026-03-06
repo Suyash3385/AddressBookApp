bbc2e0f655daa0ee881a75c30f4f6374780b676f
Suyash <suyashsharma942@gmail.com>
2026-03-06 11:42:00 +0530

    [Suyash]:Added main of address book

---
+++ AddressBookApp/src/main/java/com/addressbook/addressbookapp/AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.addressbook.addressbookapp;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to Address book");
+
+	}
+
+}
