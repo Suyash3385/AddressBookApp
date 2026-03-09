b3602e63a5d72cffbd0563ef41dc65b88e1361bd
Suyash <suyashsharma942@gmail.com>
2026-03-09 10:33:01 +0530

    [Suyash]:Added UC3 edit contact

--- AddressBookApp/src/main/java/com/addressbook/addressbookapp/model/Contact.java
+++ AddressBookApp/src/main/java/com/addressbook/addressbookapp/model/Contact.java
@@ -35,6 +35,22 @@ public class Contact {
     public String getPhoneNumber() { return phoneNumber; }
     public String getEmail() { return email; }
 
+    // Setters (Needed for UC3)
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public void setAddress(String address) { this.address = address; }
+
+    public void setCity(String city) { this.city = city; }
+
+    public void setState(String state) { this.state = state; }
+
+    public void setZip(String zip) { this.zip = zip; }
+
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public void setEmail(String email) { this.email = email; }
+
     @Override
     public String toString() {
         return firstName + " " + lastName + " | " +
